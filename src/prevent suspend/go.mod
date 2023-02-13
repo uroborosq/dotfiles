6e88eec86ab2339cb9b08d8471f618a9e9f968e9
Dmitriy Tikhomirov <uroborosq@ya.ru>
2023-02-13 18:42:23 +0300

    feat: new implementation of preventing suspend

---
+++ src/prevent suspend/go.mod
@@ -0,0 +1,5 @@
+module prevent-suspend
+
+go 1.20
+
+require golang.org/x/exp v0.0.0-20230212135524-a684f29349b6
---
+++ src/prevent suspend/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"golang.org/x/exp/slices"
+)
+
+func main() {
+	exec.Command("sudo", "systemctl", "unmask", "suspend.target").Run()
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGTERM)
+	isSuspendDisabled := false
+	for {
+		select {
+		case <-sigs:
+			fmt.Println("Interrupt signal recevied, restoring defaults...")
+			if isSuspendDisabled {
+				exec.Command("sudo", "systemctl", "unmask", "suspend.target").Run()
+			}
+			os.Exit(0)
+		default:
+			cmd := exec.Command("playerctl", "status")
+			title, _ := cmd.Output()
+			fmt.Printf("Current status %s", string(title))
+			if slices.Equal(title, []byte("Playing\n")) {
+				if !isSuspendDisabled {
+					exec.Command("sudo", "systemctl", "mask", "suspend.target").Run()
+					fmt.Println("suspend disabled")
+					isSuspendDisabled = true
+				}
+			} else if isSuspendDisabled {
+				exec.Command("sudo", "systemctl", "unmask", "suspend.target").Run()
+				isSuspendDisabled = false
+				fmt.Println("suspend enabled")
+			}
+			time.Sleep(1 * time.Second)
+		}
+	}
+}
