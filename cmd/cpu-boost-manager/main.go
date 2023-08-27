c88333a4cb530e67fe041b3bcd87e28924209833
Dmitriy Tikhomirov <uroborosq@ya.ru>
2023-08-27 14:44:32 +0300

    add config parsing in cpuboostmanager

--- cmd/cpu-boost-manager/main.go
+++ cmd/cpu-boost-manager/main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/viper"
+
 	"linux/pkg/battery"
 	"linux/pkg/boost"
 	"linux/pkg/logger"
@@ -16,8 +21,33 @@ type Booster interface {
 	Status() (bool, error)
 }
 
+type Policy string
+
+const (
+	Auto      Policy = "auto"
+	AlwaysOn  Policy = "always-on"
+	AlwaysOff Policy = "always-off"
+)
+
+type Config struct {
+	Policy
+}
 
 func main() {
+	homedir, err := os.UserHomeDir()
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+
+	viper.SetConfigFile(filepath.Join(homedir, ".config", "uq.conf"))
+	viper.SetConfigType("json")
+
+	if err = viper.ReadInConfig(); err != nil {
+		logger.Fatal(err.Error())
+	}
+
+	config := viper.Get("cpu-boost-manager").(map[string]interface{})
+
 	booster, err := boost.GetBooster()
 	if err != nil {
 		logger.Fatal(err.Error())
@@ -25,19 +55,25 @@ func main() {
 
 	battery := battery.NewPrimaryBattery()
 
-	for {
-		if battery.IsCharging() && booster.Status() {
-			err := booster.SetStatus(false)
-			if err != nil {
-				logger.Warn("can't switch boost mode: %s", err.Error())
+	if config["policy"] == AlwaysOn {
+		booster.SetStatus(true)
+	} else if config["policy"] == AlwaysOff {
+		booster.SetStatus(false)
+	} else {
+		for {
+			if battery.IsCharging() && booster.Status() {
+				err := booster.SetStatus(false)
+				if err != nil {
+					logger.Warn("can't switch boost mode: %s", err.Error())
+				}
+			} else if !battery.IsCharging() && !booster.Status() {
+				booster.SetStatus(true)
+				if err != nil {
+					logger.Warn("can't switch boost mode: %s", err.Error())
+				}
 			}
-		} else if !battery.IsCharging() && !booster.Status() {
-			booster.SetStatus(true)
-			if err != nil {
-				logger.Warn("can't switch boost mode: %s", err.Error())
-			}
-		}
 
-		time.Sleep(5 * time.Second)
+			time.Sleep(5 * time.Second)
+		}
 	}
 }
