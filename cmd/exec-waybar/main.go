ef67b458868b3956b6a61507ad9be32315015bad
Dmitriy Tikhomirov <uroborosq@ya.ru>
2023-08-27 14:44:31 +0300

    feat: replaced exec-waybar bash with go

--- cmd/exec-waybar/main.go
+++ cmd/exec-waybar/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"linux/pkg/logger"
 	"os"
+	"os/exec"
 	"os/signal"
 	"syscall"
 
@@ -15,20 +17,30 @@ const (
 
 func main() {
 	log.Infof("Starting EXEC-WAYBAR service")
-	
+
 	if os.Getenv("XDG_CURRENT_DESKTOP") != sway {
 		log.Fatalf("Can be used only on %s", sway)
 	}
 
 	ctx, _ := signal.NotifyContext(context.Background(), syscall.SIGHUP, syscall.SIGINT, syscall.SIGKILL, syscall.SIGQUIT)
 
+	counter := 0
+
 	for {
 		select {
-			case <-ctx.Done():
-				log.Infof("Received interrupt signal! Aboring...")
+		case <-ctx.Done():
+			log.Infof("Received interrupt signal! Aboring...")
+			os.Exit(1)
+		default:
+			err := exec.Command("waybar").Run()
+			if err != nil {
+				logger.Warn(err.Error())
+			}
+			counter++
+			if counter >= 1000 {
+				logger.Fatal("Exceeded number of attempts")
 				os.Exit(1)
-			default:
-				
+			}
 		}
 	}
 }
--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -4,6 +4,14 @@ import "github.com/juju/loggo"
 
 var logger = loggo.GetLogger(loggo.DefaultWriterName)
 
-func Info(s string, args... any) {
+func Info(s string, args ...any) {
 	logger.Infof(s, args...)
+}
+
+func Warn(s string, args ...any) {
+	logger.Warningf(s, args...)
+}
+
+func Fatal(s string, args ...any) {
+	logger.Errorf(s, args...)
 }
