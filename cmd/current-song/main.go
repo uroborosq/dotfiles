aa99fb22d28b051f32aa703b55400f586aee4b10
Dmitriy Tikhomirov <uroborosq@ya.ru>
2023-11-18 23:59:47 +0300

    feat: use awesome icons in current song + trim newlines

--- cmd/current-song/main.go
+++ cmd/current-song/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"flag"
 	"fmt"
 	"os/exec"
 	"strings"
@@ -9,9 +11,11 @@ import (
 )
 
 func main() {
+	limit := flag.Int("limit", 100, "limit output")
+	flag.Parse()
+
 	cmd := exec.Command("playerctl", "metadata", "xesam:title")
 	title, err := cmd.Output()
-
 	if err != nil {
 		fmt.Println("Ничего не вопроизводится")
 		return
@@ -20,20 +24,36 @@ func main() {
 	cmd = exec.Command("playerctl", "metadata", "xesam:artist")
 	artist, _ := cmd.Output()
 	status, _ := exec.Command("playerctl", "status").Output()
-	if slices.Equal(status, []byte("Playing\n")) {
-		fmt.Print("⏵︎ ")
-	} else if slices.Equal(status, []byte("Paused\n")) {
-		fmt.Print("⏸︎ ")
-	}
-	if len(artist) == 1 && artist[0] == 10 {
-		fmt.Println(string(title))
-	} else {
-		limit := 100
-		if len(title) > limit {
-			fmt.Println(string([]rune(string(title))[:limit]) + "..." + " | " + string(artist))
-		} else {
-			fmt.Println(strings.TrimSuffix(string(title), "\n") + " | " + strings.TrimSuffix(string(artist), "\n"))
-		}
+
+
+	artist = bytes.TrimSpace(artist)
+	title = bytes.TrimSpace(title)
+
+
+	if len(artist) > *limit {
+		
 	}
 
+	var output strings.Builder
+
+	if slices.Equal(status, []byte("Playing\n")) {
+		output.WriteString(" ")
+	} else if slices.Equal(status, []byte("Paused\n")) {
+		output.WriteString(" ")
+	}
+
+	if len(title) > *limit {
+		output.WriteString(string([]rune(string(title)[:*limit])) + "...")
+	} else {
+		output.Write(title)
+	}
+
+	if len(artist) > *limit {
+		output.WriteString(" | ")
+		output.WriteString(string([]rune(string(artist)[:*limit])) + "...")
+	} else if len(artist) != 0 {
+		output.WriteString(" | ")
+		output.Write(artist)
+	}
+	fmt.Println(output.String())
 }
