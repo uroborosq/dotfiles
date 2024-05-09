96286d7632afa792b09901f41e2f8342120b421e
Dmitriy Tikhomirov <uroborosq@ya.ru>
2024-05-10 00:59:21 +0300

    feat: add nvim configs

---
+++ configs/home/.config/nvim/lua/plugins/neotest.lua
@@ -0,0 +1,15 @@
+return {
+   {
+  "nvim-neotest/neotest",
+  dependencies = {
+    "nvim-neotest/nvim-nio",
+    "nvim-lua/plenary.nvim",
+    "antoinemadec/FixCursorHold.nvim",
+    "nvim-treesitter/nvim-treesitter"
+  }
+},
+  {
+        "nvim-neotest/neotest-go",
+    {},
+  }
+}
